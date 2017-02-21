03ccc90911ae52916d11663c4b03304ec4ab0fd0
a33584080 <chen373732786@hotmail.com>
2017-02-21 11:56:12 -0800

    0221AM1155

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,6 +25,12 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  # Disable Rails's static asset server (Apache or nginx will already do this)  
+config.serve_static_assets = true
+
+	# Compress JavaScripts and CSS  
+config.assets.compress = true
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
