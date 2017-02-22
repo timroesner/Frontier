21ea291945260c9fa7ab742e8d641e9e91945b3e
a33584080 <chen373732786@hotmail.com>
2017-02-21 16:37:43 -0800

    allow heroku auto assets precompilation

---
+++ vendor/bundle/ruby/2.2.0/bin/scss.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+IF NOT "%~f0" == "~f0" GOTO :WinNT
+@"C:\Ruby22-x64\bin\ruby.exe" "C:/Users/emily/Dropbox/Frontier/vendor/bundle/ruby/2.2.0/bin/scss" %1 %2 %3 %4 %5 %6 %7 %8 %9
+GOTO :EOF
+:WinNT
+@"C:\Ruby22-x64\bin\ruby.exe" "%~dpn0" %*
