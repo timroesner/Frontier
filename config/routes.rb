5f3e4ec28f8f69b75cf6f725d399079441f013f3
Tim Roesner <timroesner@me.com>
2017-02-26 15:25:34 -0800

    adding search

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
 
   resources :users
   resources :cars
+  resources :search
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
