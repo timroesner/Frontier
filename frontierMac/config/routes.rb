f092cd41cb2d03f2a9d9ad22134f10ac41bcbe63
Tim Roesner <timroesner@me.com>
2017-05-03 23:33:13 -0700

    Fixed log in/out procedure

--- frontierMac/config/routes.rb
+++ frontierMac/config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'search/index'
-
   get 'welcome/index'
   root 'welcome#index'
 
   resources :cars
+  resources :search
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
