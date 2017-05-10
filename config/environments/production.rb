651a8d1310fd8c032d0b472d3722c7ccf713f554
Tim Roesner <timroesner@me.com>
2017-05-10 15:17:14 -0700

    Added Request feature

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,6 +25,7 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
   config.serve_static_assets = true
+  config.assets.initialize_on_precompile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
