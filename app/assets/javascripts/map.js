a9625e99fa8096b8633ed4239ea0d6d2e70293a2
Tim Roesner <timroesner@me.com>
2017-05-10 15:10:30 -0700

    Added Request feature

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,14 +1,7 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoidGltcm9lc25lciIsImEiOiJjajJhbzEzZ2QwMDkwMzNvZ2YxZnVsbXI2In0.Dq8bsIZh_GP75xpawX3fcA';
 
-var request = new XMLHttpRequest();
-request.open("GET", "/assets/long.txt", false);
-request.send()
-var long = parseFloat(request.responseText)
-request.open("GET", "/assets/lat.txt", false);
-request.send()
-var lat = parseFloat(request.responseText)
-   
-   
+   var lat = <%= lat %>  
+   var long = <%= long %> 
 
 var map = new mapboxgl.Map({
     container: 'map',
