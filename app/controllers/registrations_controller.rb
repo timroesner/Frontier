9c9bc6075de5391941772521d786e652a172e703
Tim Roesner <timroesner@me.com>
2017-05-11 10:02:40 -0700

    Redirect after sign-up

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,9 @@
+class RegistrationsController < Devise::RegistrationsController
+  protected
+  def after_sign_up_path_for(resource)
+    '/'
+  end
+  def after_inactive_sign_up_path_for(resource)
+    '/'
+  end
+end
